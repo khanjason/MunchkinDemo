f752d0bba70199e122481cca386d8e69e757ebb7
khanjason <jasonxdecember@gmail.com>
2020-05-21 16:36:04 +0100

    Add project files.

---
+++ MunchkinDemo/MUNchkin Demo.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MunchkinDemo
+{
+    public partial class Form1 : Form
+    {
+        private Label Title;
+        ListBox presentBox;
+        private string version = "V0.1";
+        BindingList<string> registeredList = new BindingList<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetUpUI();
+        }
+
+        void SetUpUI()
+        {
+            Color Titlecolor = System.Drawing.ColorTranslator.FromHtml("#ECEFF2");
+            Title = new Label();
+            Title.Text = "MUNchkin" + " "+version;
+            Title.Font = new Font("Berlin Sans FB", 54f);
+            Title.Size = new Size(500, this.Width);
+            Title.ForeColor = Titlecolor;
+            Color background = System.Drawing.ColorTranslator.FromHtml("#7EA5DF");
+            Title.Location = new Point(this.Width, 25);
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            
+
+            this.BackColor = background;
+            this.Controls.Add(Title);
+            AddRegisterlistbox();
+        }
+
+        void AddRegisterlistbox()
+        {
+            
+
+            Color labelcolor = System.Drawing.ColorTranslator.FromHtml("#ECEFF2");
+            Color buttonColor = ColorTranslator.FromHtml("#3068BD");
+
+            Label caption1 = new Label();
+            caption1.Text = "Available Delegations";
+            caption1.Font = new Font("Berlin Sans FB", 22f);
+            caption1.Size = new Size(250, 180);
+            caption1.ForeColor = labelcolor;
+            
+            presentBox = new ListBox();
+            presentBox.Size = new System.Drawing.Size(200, 100);
+            presentBox.Location = new System.Drawing.Point(10, 250);
+            caption1.Location = new Point(presentBox.Location.X, presentBox.Location.Y-presentBox.Height);
+            presentBox.Items.AddRange(new object[]{"United Kingdom",
+        "Saudi Arabia", "USA", "France", "China",
+        "Spain"});
+
+            Label caption2 = new Label();
+            caption2.Text = "Registered Delegations";
+            caption2.Font = new Font("Berlin Sans FB", 22f);
+            caption2.Size = new Size(250, 180);
+            caption2.ForeColor = labelcolor;
+
+            ListBox registeredBox = new ListBox();
+            registeredBox.DataSource = registeredList;
+            registeredBox.Size = new Size(200, 100);
+            registeredBox.Location = new Point(270, 250);
+            caption2.Location = new Point(registeredBox.Location.X, registeredBox.Location.Y - registeredBox.Height);
+
+            Button addtoRegisterButton = new Button();
+            addtoRegisterButton.Text = ">";
+            addtoRegisterButton.ForeColor = labelcolor;
+            addtoRegisterButton.BackColor = buttonColor;
+            addtoRegisterButton.Location = new Point(presentBox.Location.X, presentBox.Location.Y + presentBox.Height);
+            addtoRegisterButton.Click += new System.EventHandler(this.AddToRegister);
+
+
+            this.Controls.Add(addtoRegisterButton);
+
+            this.Controls.Add(presentBox);
+            this.Controls.Add(caption1);
+
+            this.Controls.Add(registeredBox);
+            this.Controls.Add(caption2);
+        }
+
+        void AddToRegister( object sender, EventArgs e)
+        {
+            string curItem = presentBox.SelectedItem.ToString();
+            registeredList.Add(curItem);
+
+        }
+    }
+}
---
+++ MunchkinDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MunchkinDemo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            Form register = new Form1();
+            //set up UI
+            
+            
+            Application.Run(register);
+        }
+
+
+
+    }
+}
